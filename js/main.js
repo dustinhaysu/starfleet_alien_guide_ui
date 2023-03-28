821ef48f99ff4d9cf1e87132a30938f5fe7320e4
Dustin Hays <hays.dustin.u@gmail.com>
2023-03-28 05:44:09 -0700

    end of VOD

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/*set this up as a drop down select menu in HTML*/
+
 document.getElementById('getButton').addEventListener('click', apiRequest)
 
 
@@ -8,6 +10,16 @@ async function apiRequest(){
         const response = await fetch(`https://dustin-star-trek-api.herokuapp.com/api/${alienName}`)
         const data = await response.json()
         console.log(data)
+
+        document.getElementById("alienImage").src = data.image
+        document.getElementById('alienCaption').innerText = data.speciesName
+
+        document.getElementById('alienName').innerText = data.speciesName
+        document.getElementById("alienWorld").innerText = data.homeworld
+        document.getElementById("alienFeatures").innerText = data.features
+        document.getElementById("alienFacts").innerText = data.interestingFact
+        document.getElementById("alienExamples").innerText = data.notableExamples
+
 
     } catch(error) {
         console.log(error)
